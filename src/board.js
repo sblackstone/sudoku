2be2a6bac2355c1315636df4128ffa835b731404
Stephen Blackstone <sblackstone@gmail.com>
2020-02-13 22:38:57 -0500

    New functions for sudoku

--- src/board.js
+++ src/board.js
@@ -1,14 +1,66 @@
 
 class Cell {
   constructor(value) {
-    this.value = value || null;
+    this.value = value || 99;
     this.marks = [1,1,1,1,1,1,1,1,1];
   }
 }
 
 class Board {
 
+  valueInRow(x, value) {
+    for (let j = 0; j < 9; j++) {
+      if (this.rows[x][j].value === value) {
+        return true;
+      }
+    }
+    return(false);
+  }
+
+  valueInCol(y, value) {
+    for (let i = 0; i < 9; i++) {
+      if (this.rows[i][y].value === value) {
+        return true;
+      }
+    }
+    return(false);
+  }
+
+  valueInBox(n, value) {
+    const boxes = this.squareBoxes[n];
+    for (let i = 0; i < boxes.length; i++) {
+      const box = boxes[i];
+      const x = box[0];
+      const y = box[1];
+      if (this.rows[x][y].value === value) {
+        return true;
+      }
+    }
+
+    return(false);
+  }
+
+  boxNumberForSquare(i,j) {
+    return Math.floor(i / 3) * 3 + Math.floor(j /3);
+  }
+
+  cacheSquareBoxes() {
+    this.squareBoxes = [];
+
+    for (let i = 0; i < 9; i++) {
+      this.squareBoxes[i] = [];
+    }
+
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        const boxNumber = this.boxNumberForSquare(i,j);
+        this.squareBoxes[boxNumber].push([i,j]);
+      }
+    }
+  }
   constructor() {
+    window.model = this;
+    this.cacheSquareBoxes();
     this.rows = [];
     for (let i = 0; i < 9; i++) {
       const row = [];
@@ -18,6 +70,13 @@ class Board {
       this.rows.push(row);
     }
 
+
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        this.rows[i][j].value = this.boxNumberForSquare(i,j) + 1;
+      }
+    }
+    return;
     this.rows[0][2].value = 6;
     this.rows[0][4].value = 1;
     this.rows[1][1].value = 2;
@@ -48,7 +107,7 @@ class Board {
     this.rows[8][2].value = 4;
     this.rows[8][4].value = 9;
     this.rows[8][8].value = 6;
-    
+
 
 
 
