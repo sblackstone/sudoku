d8acdf2bab387c2afa9bbec189dd88b8040d2e73
Stephen Blackstone <sblackstone@gmail.com>
2020-02-15 21:42:09 -0500

    new ui elements

--- src/controls.mjs
+++ src/controls.mjs
@@ -1,6 +1,14 @@
 import React from 'react';
 import './scss/controls.scss';
 
+const NumberButtons = function(props) {
+  const result = [];
+  for (let i = 1; i < 10; i++) {
+    result.push(<div className="numberButton">{i}</div>);
+  }
+  return result;
+}
+
 export class Controls extends React.Component {
 
   constructor(props) {
@@ -8,7 +16,9 @@ export class Controls extends React.Component {
   }
 
   render() {
-    return (<div>Hello World</div>);
+    return (<div>
+             <NumberButtons />
+            </div>);
   }
 
 }
